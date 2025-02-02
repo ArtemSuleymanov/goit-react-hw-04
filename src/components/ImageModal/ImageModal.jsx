f183b0d50964614470c81b541fc0f677e3bc783a
Artem <suleymanov2007artem@gmail.com>
2025-02-02 23:38:13 +0200

    edit structure and add css styles

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -4,12 +4,6 @@ import Modal from "react-modal";
 const ImageModal = ({ isOpen, closeModal, image }) => {
   if (!image) return null;
 
-  const handleOverlayClick = (e) => {
-    if (e.target === e.currentTarget) {
-      closeModal();
-    }
-  };
-
   return (
     <Modal
       isOpen={isOpen}
@@ -19,12 +13,23 @@ const ImageModal = ({ isOpen, closeModal, image }) => {
       contentLabel="Image Modal"
       ariaHideApp={false}
     >
-      <div onClick={handleOverlayClick} style={{ cursor: "pointer" }}>
+      <div className={s.modalBody}>
         <img
           src={image.urls.regular}
           alt={image.alt_description}
           className={s.modalImage}
         />
+        <div className={s.info}>
+          <p>
+            <strong>Author:</strong> {image.user.name}
+          </p>
+          <p>
+            <strong>Likes:</strong> {image.likes}
+          </p>
+          <p>
+            <strong>Description:</strong> {image.description || "Empty"}
+          </p>
+        </div>
       </div>
     </Modal>
   );
